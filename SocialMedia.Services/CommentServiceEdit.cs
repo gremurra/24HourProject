9a930e5b1d64bf7b23ab2da61b37fb39c33151a8
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-04-21 09:16:05 -0400

    Gurpreet Morning

---
+++ SocialMedia.Services/CommentServiceEdit.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.Services
+{
+    public class CommentServiceEdit
+    {
+        public object Id { get; internal set; }
+        public object Text { get; internal set; }
+        public object Author { get; internal set; }
+        public object CommentPost { get; internal set; }
+    }
+}
