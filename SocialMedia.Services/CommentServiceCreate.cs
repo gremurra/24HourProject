e5775ef7384e35ddcae00648b8dcf51a453e90d0
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-04-21 10:16:33 -0400

    2nd morning push

--- SocialMedia.Services/CommentServiceCreate.cs
+++ SocialMedia.Services/CommentServiceCreate.cs
@@ -5,5 +5,6 @@
         public object Text { get; internal set; }
         public object Author { get; internal set; }
         public object CommentPost { get; internal set; }
+        public object ReplyComment { get; internal set; }
     }
 }
